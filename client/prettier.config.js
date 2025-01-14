31c73f52b425b3f99375bcb077234c5879d8deb3
PlatinaSB <59110147+PlatinaSB@users.noreply.github.com>
2025-01-14 18:43:34 +0700

    made someting to client

---
+++ client/prettier.config.js
@@ -0,0 +1,5 @@
+/** @type {import('prettier').Config} */
+export const plugins = ["prettier-plugin-tailwindcss"];
+export const tailwindAttributes = ["theme"];
+export const tailwindFunctions = ["twMerge", "createTheme"];
+  
